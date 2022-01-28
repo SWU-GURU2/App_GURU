a1edfd1db670111d22ff554a8d9df3e150d7999d
withtaylors <withtaylors@naver.com>
2022-01-28 11:40:42 +0900

    22.01.28/오전11시40분 수정

---
+++ app/src/main/java/com/example/guru2/WaterEdit.kt
@@ -0,0 +1,11 @@
+22package com.example.guru2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WaterEdit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_water_edit)
+    }
+}

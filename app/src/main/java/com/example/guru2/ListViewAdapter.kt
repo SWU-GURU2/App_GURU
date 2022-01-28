de3f2f936db15b2fba0eb24f3ab3a2303992df74
ahyoungss <sallyajoa@naver.com>
2022-01-29 00:34:20 +0900

    ahyoung_health

---
+++ app/src/main/java/com/example/guru2/ListViewAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.guru2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.custom_list_item.view.*
+
+class ListViewAdapter(private val items: MutableList<ListViewItem>): BaseAdapter() {
+    override fun getCount(): Int = items.size
+    override fun getItem(position: Int): ListViewItem = items[position]
+    override fun getItemId(position: Int): Long = position.toLong()
+    override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
+        var convertView = view
+        if (convertView == null) convertView = LayoutInflater.from(parent?.context).inflate(R.layout.custom_list_item, parent, false)
+        val item: ListViewItem = items[position]
+        convertView!!.checkIcon.setImageDrawable(item.icon)
+        convertView.fieldText.text = item.title
+        convertView.exerciseText.text = item.subTitle
+        return convertView
+    }
+}
+

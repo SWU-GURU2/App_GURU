59299f0de37205a33f9a80c536d65e9f4aeaac62
withtaylors <withtaylors@naver.com>
2022-02-06 00:07:17 +0900

    0206/1

--- app/src/main/java/com/example/guru2/MainActivity.kt
+++ app/src/main/java/com/example/guru2/MainActivity.kt
@@ -11,12 +11,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-
     var auth: FirebaseAuth? = null
     private lateinit var googleSignInClient: GoogleSignInClient
-
     lateinit var waterButton: Button
-    lateinit var food: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,64 +38,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         //음수량 수정 화면
-         val waterButton = findViewById<Button>(R.id.waterButton)
+        val waterButton = findViewById<Button>(R.id.waterButton)
 
-//        food.setOnClickListener {
-//            var intent = Intent(this, MealChoiceActivity::class.java) //인텐트 생성
-//            startActivity(intent)
-//        }
         //버튼 클릭시 할 행동
         waterButton.setOnClickListener {
-            var intent11 = Intent(this, WaterEdit::class.java) //인텐트 생성
-            startActivity(intent11)
+            var intent = Intent(this, WaterEdit::class.java) //인텐트 생성
+            startActivity(intent)
         }
-
-
-//        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//            // 1. xml을 이용한 메뉴 만들기
-//            menuInflater.inflate(R.menu.option_menu, menu)
-//
-//            // 2. 코드를 이용한 메뉴 만들기
-//            menu?.add(Menu.NONE, Menu.FIRST + 1, Menu.NONE, "코드메뉴1")
-//            menu?.add(Menu.NONE, Menu.FIRST + 2, Menu.NONE, "코드메뉴2")
-//
-//            var sub: Menu? = menu?.addSubMenu("메뉴3")
-//            sub?.add(Menu.NONE, Menu.FIRST + 3, Menu.NONE, "코드메뉴3-1")
-//            sub?.add(Menu.NONE, Menu.FIRST + 4, Menu.NONE, "코드메뉴3-2")
-//
-//            // true로 해야 메뉴가 나타난다.
-//            return true
-//            // false는 안나옴
-//            return false
-//        }
-//
-//        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//            // 1. xml을 이용한 메뉴 만들기
-//            when (item?.itemId) {
-//                R.id.item1
-//                -> tv1.text = "1"
-//                R.id.Item2_1
-//                -> tv1.text = "2-1"
-//                R.id.Item2_2
-//                -> tv1.text = "2-2"
-//                R.id.item3
-//                -> tv1.text = "3"
-//            }
-//
-//            // 2. 코드를 이용한 메뉴 만들기
-//            when (item?.itemId) {
-//                Menu.FIRST + 1
-//                -> tv1.text = "1"
-//                Menu.FIRST + 2
-//                -> tv1.text = "2"
-//                Menu.FIRST + 3
-//                -> tv1.text = "3-1"
-//                Menu.FIRST + 4
-//                -> tv1.text = "3-2"
-//            }
-//
-//            return super.onOptionsItemSelected(item)
-//        }
-
     }
 }
--- app/src/main/java/com/example/guru2/WaterList.kt
+++ app/src/main/java/com/example/guru2/WaterList.kt
@@ -8,65 +8,95 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 
 class WaterList : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        lateinit var WaterDBManger: WaterDBManger
-        lateinit var sqlitedb: SQLiteDatabase
-        lateinit var layout: LinearLayout
+    lateinit var WaterDBManger: WaterDBManger
+    lateinit var sqlitedb: SQLiteDatabase
+    lateinit var layout: LinearLayout
+    lateinit var calChoiceButton: Button
+    lateinit var calendarChoice: CalendarView
+    lateinit var scrollWater: ScrollView
 
+    var selectYear: Int = 0
+    var selectMonth: Int = 0
+    var selectDate: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_water_list)
 
-        WaterDBManger = WaterDBManger(this, "waterDB", null, 1 )
+        WaterDBManger = WaterDBManger(this, "waterlist", null, 1)
         sqlitedb = WaterDBManger.readableDatabase
 
         layout = findViewById(R.id.waterlist)
+        scrollWater = findViewById(R.id.scrollWater)
+        calChoiceButton = findViewById(R.id.calChoiceButton)
+        calendarChoice = findViewById(R.id.calendarChoice)
 
-        var cursor: Cursor
-        cursor = sqlitedb.rawQuery("SELECT * FROM waterlist", null)
+        calendarChoice.setOnDateChangeListener { view, year, month, date ->
+            selectYear = year
+            selectMonth = month + 1
+            selectDate = date
+            var alarm: String =
+                selectYear.toString() + "년 " + selectMonth.toString() + "월 " + selectDate.toString() + "일 " + "기록을 확인합니다."
+            calChoiceButton.setOnClickListener {
+                Toast.makeText(applicationContext, alarm, Toast.LENGTH_SHORT).show()
+                calChoiceButton.visibility = View.GONE
+                calendarChoice.visibility = View.GONE
+                scrollWater.visibility = ScrollView.VISIBLE
+                layout.visibility = LinearLayout.VISIBLE
 
-        var num: Int = 0
-        while (cursor.moveToNext()){
-            var str_year = cursor.getString(cursor.getColumnIndex("year")).toString()
-            var str_month = cursor.getString(cursor.getColumnIndex("month")).toString()
-            var str_date = cursor.getString(cursor.getColumnIndex("date")).toString()
-            var str_waterml = cursor.getString(cursor.getColumnIndex("water")).toString()
+                var cursor: Cursor
+                cursor = sqlitedb.rawQuery(
+                    "SELECT * FROM waterlist WHERE year = " + selectYear + " AND month = " + selectMonth + " AND date = " + selectDate + ";",
+                    null
+                )
 
-            var layout_item: LinearLayout = LinearLayout(this)
-            layout_item.orientation = LinearLayout.VERTICAL
-            layout_item.id = num
+                var num: Int = 0
+                while (cursor.moveToNext()) {
+                    var str_year = cursor.getString(cursor.getColumnIndex("year")).toString()
+                    var str_month = cursor.getString(cursor.getColumnIndex("month")).toString()
+                    var str_date = cursor.getString(cursor.getColumnIndex("date")).toString()
+                    var str_waterml = cursor.getString(cursor.getColumnIndex("water")).toString()
 
-            var tvCalView: TextView = TextView(this)
-            tvCalView.text = str_year + "년" + str_month + "월" + str_date + "일"
-            tvCalView.textSize = 25F
-            tvCalView.setBackgroundColor(Color.WHITE)
-            tvCalView.setTextColor(Color.BLACK)
-            layout_item.addView(tvCalView)
+                    var layout_item: LinearLayout = LinearLayout(this)
+                    layout_item.orientation = LinearLayout.VERTICAL
+                    layout_item.id = num
 
-            var tvWater: TextView = TextView(this)
-            tvWater.text = str_waterml
-            tvWater.textSize = 25F
-            tvWater.setBackgroundColor(Color.WHITE)
-            tvWater.setTextColor(Color.BLACK)
-            layout_item.addView(tvWater)
+                    var tvCalView: TextView = TextView(this)
+                    tvCalView.text = str_year + "년" + str_month + "월" + str_date + "일"
+                    tvCalView.textSize = 25F
+                    tvCalView.setBackgroundColor(Color.WHITE)
+                    tvCalView.setTextColor(Color.BLACK)
+                    layout_item.addView(tvCalView)
 
-            layout_item.setOnClickListener{
-                val intent = Intent(this, WaterEdit::class.java)
-                intent.putExtra("intent_year", str_year.toString())
-                intent.putExtra("intent_month", str_month.toString())
-                intent.putExtra("intent_date", str_date.toString())
-                intent.putExtra("intent_ml", str_waterml)
-                startActivity(intent)
+                    var tvWater: TextView = TextView(this)
+                    tvWater.text = str_waterml
+                    tvWater.setBackgroundColor(Color.WHITE)
+                    tvWater.setTextColor(Color.BLACK)
+                    layout_item.addView(tvWater)
+
+                    layout_item.setOnClickListener {
+                        val intent = Intent(this, WaterEdit3::class.java)
+                        intent.putExtra("intent_year", selectYear.toString())
+                        intent.putExtra("intent_month", selectMonth.toString())
+                        intent.putExtra("intent_date", selectDate.toString())
+                        intent.putExtra("intent_year", str_year.toString())
+                        intent.putExtra("intent_month", str_month.toString())
+                        intent.putExtra("intent_date", str_date.toString())
+                        intent.putExtra("intent_ml", str_waterml)
+                        startActivity(intent)
+                    }
+                    layout.addView(layout_item)
+                    num++
+                }
+                cursor.close()
+                sqlitedb.close()
+                WaterDBManger.close()
             }
-            layout.addView(layout_item)
-            num++
         }
-        cursor.close()
-        sqlitedb.close()
-        WaterDBManger.close()
     }
 
     //메뉴바 추가
@@ -89,7 +119,7 @@ class WaterList : AppCompatActivity() {
                 return true
             }
             R.id.water -> {
-                val mintent = Intent(this, WaterEdit::class.java)
+                val mintent = Intent(this, WaterEdit3::class.java)
                 startActivity(mintent)
                 return true
             }

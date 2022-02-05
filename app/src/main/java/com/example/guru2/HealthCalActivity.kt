b29b60e568c8ac20d6664dd42752d3ae94c26372
ahyoungss <sallyajoa@naver.com>
2022-02-05 20:43:21 +0900

    0205_2

--- app/src/main/java/com/example/guru2/HealthCalActivity.kt
+++ app/src/main/java/com/example/guru2/HealthCalActivity.kt
@@ -59,19 +59,19 @@ class HealthCalActivity : AppCompatActivity() {
 
 
         barChart.run {
-            description.isEnabled = false // 차트 옆에 별도로 표기되는 description을 안보이게 설정 (false)
+            description.isEnabled = false
             setMaxVisibleValueCount(5) // 최대 보이는 그래프 개수를 5개로 지정
-            setPinchZoom(false) // 핀치줌(두손가락으로 줌인 줌 아웃하는것) 설정
-            setDrawBarShadow(false) //그래프의 그림자
-            setDrawGridBackground(false)//격자구조 넣을건지
+            setPinchZoom(false)
+            setDrawBarShadow(false)
+            setDrawGridBackground(false)
             axisLeft.run { //왼쪽 축. 즉 Y방향 축을 뜻한다.
                 axisMaximum = 301f //50 위치에 선을 그리기 위해 51f로 맥시멈값 설정
                 axisMinimum = 0f // 최소값 0
                 granularity = 100f // 10 단위마다 선을 그리려고 설정.
-                setDrawLabels(true) // 값 적는거 허용 (0, 50, 100)
+                setDrawLabels(true)
                 setDrawGridLines(true) //격자 라인 활용
                 setDrawAxisLine(false) // 축 그리기 설정
-                axisLineColor = ContextCompat.getColor(context,R.color.design_default_color_secondary_variant) // 축 색깔 설정
+                axisLineColor = ContextCompat.getColor(context,R.color.main_blue) // 축 색깔 설정
                 gridColor = ContextCompat.getColor(context,R.color.design_default_color_on_secondary) // 축 아닌 격자 색깔 설정
                 textColor = ContextCompat.getColor(context,R.color.design_default_color_on_secondary) // 라벨 텍스트 컬러 설정
                 textSize = 13f //라벨 텍스트 크기
@@ -92,7 +92,7 @@ class HealthCalActivity : AppCompatActivity() {
         }
 
         var set = BarDataSet(entries,"DataSet") // 데이터셋 초기화
-        set.color = ContextCompat.getColor(applicationContext!!,R.color.design_default_color_secondary_variant) // 바 그래프 색 설정
+        set.color = ContextCompat.getColor(applicationContext!!,R.color.main_blue) // 바 그래프 색 설정
 
         val dataSet :ArrayList<IBarDataSet> = ArrayList()
         dataSet.add(set)
